435f2343e492610cf09e22cd6fafa38e169b1fdb
Sabarivasan A <sabari200727@gmail.com>
2026-07-18 20:59:24 +0530

    Create number.java

---
+++ DAY - 7/number.java
@@ -0,0 +1,12 @@
+package day7;
+import java.util.Scanner;
+public class number {
+    public static void main(String[] args) {
+        Scanner sr = new Scanner(System.in);
+        String []letter= {"sabari","nit","suthish"};
+
+        for(String s:letter) {
+            System.out.println(s);
+        }
+    }
+}
